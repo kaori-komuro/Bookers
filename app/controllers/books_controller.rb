f7e6829b45b0c461daab5450c06772f87f840ed0
unknown <i1i1.00263@gmail.com>
2018-11-15 18:17:21 +0900

    [Finish]4-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,45 @@
+class BooksController < ApplicationController
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def index
+  end
+
+  def new
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+     @book = Book.new(book_params)
+      if @book.save
+        flash[:notice] = "Book was successfully created."
+        redirect_to book_path(@book.id)
+      else
+        render("/books/new")
+      end
+  end
+
+  def edit
+      @book = Book.find(params[:id])
+  end
+
+  def destroy
+      @book = Book.find(params[:id])
+      @book.destroy
+      redirect_to new_book_path
+  end
+
+  def update
+      @book = Book.find(params[:id])
+      @book.update(book_params)
+      redirect_to new_book_path
+  end
+
+    private
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
+end
